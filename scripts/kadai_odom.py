ae2e3a46c35d735e43881c232309053984f6084d
MickeyKen <mickey333ff@gmail.com>
2020-03-24 02:50:30 -0700

    [Edit]

--- scripts/kadai_odom.py
+++ scripts/kadai_odom.py
@@ -28,10 +28,8 @@ if __name__ == '__main__':
         data = get_odom()
 
         print ("current position: ", data.pose.pose.position.x)
-        print data.pose.pose.position.x - x_start.pose.pose.position.x
 
         if (data.pose.pose.position.x - x_start.pose.pose.position.x) > 1.0:
-            print "in"
             move.linear.x = 0.0
             pub_move.publish(move)
 
--- scripts/kadai_scan.py
+++ scripts/kadai_scan.py
@@ -6,18 +6,24 @@ from geometry_msgs.msg import Twist
 import time
 def callback(data):
     move = Twist()
-    move.linear.x=0.1
+    move.linear.x=0.3
 
     scan_data = np.array(data.ranges)
     print ("scan data: ", scan_data[0])
 
     min_data = 1.0
 
-    if scan_data[0] < 0.5:
+    if scan_data[30] < 0.5:
         move.linear.x=0
-        move.angular.z = 0.3
+        move.angular.z=-1.57
         pub_move.publish(move)
         time.sleep(1)
+    elif scan_data[320] < 0.5:
+        move.linear.x=0
+        move.angular.z=1.57
+        pub_move.publish(move)
+        time.sleep(1)
+        
     pub_move.publish(move)
 
 if __name__ == '__main__':
